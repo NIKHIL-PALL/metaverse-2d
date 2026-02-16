4ffe15d37323d83830434dfcd4e51da0e68151c2
Pall Nikhil <pallnikhil@Palls-MacBook-Air.local>
2026-02-16 20:43:19 +0530

    mysql connection

---
+++ backend/databases/mysql.js
@@ -0,0 +1,16 @@
+import mysql from "mysql2/promise";
+import config from "../config/config.js";
+
+const pool = mysql.createPool({
+  host: config.mysql.host,
+  user: config.mysql.user,
+  password: config.mysql.password,
+  database: config.mysql.database,
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0,
+  enableKeepAlive: true,
+  keepAliveInitialDelay: 10000,
+});
+
+export default pool;
